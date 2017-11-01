23e6e823774f21a44371ee822a483b74da38bdc2
Martin Iotchev <m.iotchev@gmail.com>
2017-11-01 15:46:35 -0400

    feat: Main select UI done

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,6 @@ router.get('/', function(req, res, next) {
 	}
 });
 
-
 /**
  * AUTH BELOW
  */
@@ -47,6 +46,10 @@ router.post('/login', function(req, res, next){
 		res.redirect('/login');
 	}
 });
+
+
+
+
 
 module.exports = router;
 
